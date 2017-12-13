669c3f73c619a71dd3bda97548e6567badda8370
Lee Byeongdo <Byeongdo.lee@purpleworks.co.kr>
2017-12-13 15:53:00 +0900

    update button click event

--- app/src/main/java/com/purpleworks/bdit/MainActivity.kt
+++ app/src/main/java/com/purpleworks/bdit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.purpleworks.bdit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         text_one.text = "I am master"
 
+        button.setOnClickListener {
+            imagePopup.visibility = View.VISIBLE
+        }
     }
 }
