61c252c8451adf9ad620e014849182a81e5459fd
Lee Byeongdo <Byeongdo.lee@purpleworks.co.kr>
2017-12-13 11:04:38 +0900

    commit

--- app/src/main/java/com/purpleworks/bdit/MainActivity.kt
+++ app/src/main/java/com/purpleworks/bdit/MainActivity.kt
@@ -2,11 +2,15 @@ package com.purpleworks.bdit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text_one.text = "오늘 신동형님께서 스타벅스 커피 사주심!"
+
     }
 }
